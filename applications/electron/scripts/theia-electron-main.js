6ce8285531cef819fde38a657294920151a0ab00
Guilherme I F L Weizenmann <g.weizenmann@gmail.com>
2023-10-17 00:18:53 -0300

    Add portable mode support

--- applications/electron/scripts/theia-electron-main.js
+++ applications/electron/scripts/theia-electron-main.js
@@ -1,5 +1,18 @@
 const path = require('path');
 const os = require('os');
+const fs = require('fs')
+const { app } = require('electron');
+
+const productName = 'eos-thesis';
+const portable = fs.existsSync(path.resolve(process.execPath, `../data/.${productName}.portable`))
+if (portable) {
+    const userDataPath = path.resolve(app.getAppPath(), '../../', 'data/userData');
+    const pluginsPath = path.resolve(app.getAppPath(), '../../', 'data/plugins');
+
+    [userDataPath, pluginsPath].forEach(p => fs.existsSync(p) ? fs.mkdirSync(p, {recursive : true}) : null)
+
+    app.setPath('userData', userDataPath);
+}
 
 // Update to override the supported VS Code API version.
 // process.env.VSCODE_API_VERSION = '1.50.0'
@@ -9,7 +22,7 @@ process.env.THEIA_DEFAULT_PLUGINS = `local-dir:${path.resolve(__dirname, '../', 
 
 // Lookup inside the user's home folder for more plugins, and accept user-defined paths.
 process.env.THEIA_PLUGINS = [
-    process.env.THEIA_PLUGINS, `local-dir:${path.resolve(os.homedir(), '.theia-blueprint', 'plugins')}`,
+    process.env.THEIA_PLUGINS, (portable ? `local-dir:${pluginsPath}` : `local-dir:${path.resolve(os.homedir(), `.${productName}`, 'plugins')}`),
 ].filter(Boolean).join(',');
 
 // Handover to the auto-generated electron application handler.
--- theia-extensions/product/src/node/theia-blueprint-variables-server.ts
+++ theia-extensions/product/src/node/theia-blueprint-variables-server.ts
@@ -14,16 +14,21 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  ********************************************************************************/
 
+import * as fs from 'fs';
 import * as os from 'os';
 import * as path from 'path';
 import { injectable } from '@theia/core/shared/inversify';
 import { FileUri } from '@theia/core/lib/node/file-uri';
 import { EnvVariablesServerImpl } from '@theia/core/lib/node/env-variables';
 
+const productName = 'eos-thesis';
+const portableDirectory = path.resolve(process.execPath, '../data/server');
+const portable = fs.existsSync(path.resolve(process.execPath, `../data/.${productName}.portable`))
+
 @injectable()
 export class TheiaBlueprintEnvVariableServer extends EnvVariablesServerImpl {
 
-    protected readonly _configDirUri: string = FileUri.create(path.join(os.homedir(), '.theia-blueprint')).toString(true);
+    protected readonly _configDirUri: string = FileUri.create(portable ? portableDirectory : path.join(os.homedir(), `.${productName}`)).toString(true);
 
     async getConfigDirUri(): Promise<string> {
         return this._configDirUri;
